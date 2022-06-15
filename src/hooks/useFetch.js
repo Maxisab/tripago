1d38e302e717b2b4b3bf73eb2ab485093a6af348
Max Sabate <max.sabate@gmail.com>
2022-06-14 21:30:06 -0700

    added error handling and mitigated infinite loops

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,48 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
-export const useFetch = (url) => {
+export const useFetch = (url, _options) => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(null)
+
+  //use userRef to wrap an object/array argument
+  //which is a useEffect dependency
+  const options = useRef(_options).current
 
   useEffect(() => {
+    console.log(options)
+    const controller = new AbortController()
+
     const fetchData = async () => {
       setIsPending(true)
 
-      const res = await fetch(url)
-      const json = await res.json()
+      try {
+        const res = await fetch(url, { signal: controller.signal })
+        if (!res.ok) {
+          throw new Error(res.statusText)
+        }
+        const json = await res.json()
 
-      setIsPending(false)
-      setData(json)
+        setIsPending(false)
+        setData(json)
+        setError(null)
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log('the fetch was aborted')
+        } else {
+          setIsPending(false)
+          setError('Could no fetch the data')
+        }
+      }
     }
 
     fetchData()
-  }, [url])
 
-  return { data, isPending }
+    return () => {
+      controller.abort()
+    }
+  }, [url, options])
+
+  return { data, isPending, error }
 
 }
